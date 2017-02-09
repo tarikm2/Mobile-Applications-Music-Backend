566c6a60ee823147354047d37b5a0d9a3959186f
tarikm2 <tarikm2@uw.edu>
2017-02-09 01:48:02 -0800

    added user editing, more security and validation, user deletion, etc. the api is now close to a full fledged CRUD however it could use a few little polishing features and better doccumentation

--- app.js
+++ app.js
@@ -29,7 +29,53 @@ app.use(sessions({
     activeDuration: 5 * 60 * 1000,        //if expiresIn < activeDuration, session extended this much
 }));
 
-//TODO: custom middleware for handling session data
+
+//Custom middleware for handling session data
+app.use((req, res, next) => {
+    //if there is a user in the current session from this request
+    if(req.session && req.session.user) {
+	//try to get the user from our database
+	User.findOne({email: req.session.user.email}, (err, user) => {
+	    //if the session user matched with one from our database
+	    if(user) {
+		console.log("we are in the session middlewear");
+		//this is to keep route code neat by updating the incoming requests user
+		//object with the session info.
+		//set the user of this request to the sessions user
+		req.user = user;
+		
+		//security: remove uneccesary sensitive information
+		 req.user.password = undefined;
+		console.log(req.user);
+		//security: reset the session user to the user clear of sesitive info
+		req.session.user = user;
+		res.locals.user = user;     		//QUESTION: what are locals, really?
+	    }
+	    next();    //call the next process
+	});
+    } else {
+	next();
+    }
+});
+
+//Custom middlewear to require a user session for certain functionality
+function requireLogin(req, res, next) {
+    //if no user session is found in this request
+    if(!req.user) {
+	res.send({error: "Error: cannot access because no user is logged in. please login"});
+    } else {
+	next();
+    }
+};
+
+//custom middleware to reqire a user is logged in
+function requireLogout(req, res, next) {
+    if(req.user) {
+	res.send({error: "Error: user currently signed in, please sign out"});
+    } else {
+	next();
+    }
+};
 
 /*
   DATABASE:
@@ -61,7 +107,6 @@ var songSchema = Schema({
 var Song = mongoose.model("Song", songSchema); 
 var User = mongoose.model("User", userSchema);
 
-console.log(CREDENTIALS.DB.USER + " " + CREDENTIALS.DB.PSWD);
 mongoose.connect("mongodb://"        //connect to our local database
 		 + CREDENTIALS.DB.USER 
 		 + ":" + CREDENTIALS.DB.PSWD 
@@ -72,12 +117,12 @@ mongoose.connect("mongodb://"        //connect to our local database
 */
 
 //route for registering a user. see user schema for what should be passed.
-app.post("/register", (req, res) => {
+app.post("/register", requireLogout, (req, res) => {
     //hash and salt our passwords like good people 
     var pHash = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
     var user = new User({
 	firstName: req.body.firstName,
-	lastnName: req.body.lastName,
+	lastName: req.body.lastName,
 	userName: req.body.userName,
 	email: req.body.email,
 	password: pHash,
@@ -101,7 +146,8 @@ app.post("/register", (req, res) => {
     });   
 });
 
-app.post("/login", (req, res) => {
+
+app.post("/login", requireLogout, (req, res) => {
     User.findOne({ email: req.body.email}, (err, user) => {
 	//if no user is found for this requrest, respond with error
 	if(!user) {
@@ -109,7 +155,8 @@ app.post("/login", (req, res) => {
 	} else {
 	    console.log(user);
 	    //if the password matches our users password...
-	    if(bcrypt.compareSync(req.body.password, user.password)) {
+	    if(req.body.password 
+	       && bcrypt.compareSync(req.body.password, user.password)) {
 		//set the sessions user appropriately
 		req.session.user = user;
 		res.json({status: "Success", email: user.email});
@@ -122,16 +169,63 @@ app.post("/login", (req, res) => {
 });
 
 
-app.get('/logout', (req, res) => {
+app.get('/logout', requireLogin, (req, res) => {
     req.session.reset();
     res.json({message: "you have logged out"});
 });
 
-/*
-app.get('/userinfo', (req, res) => {
-    if (req.session)
+
+app.delete('/delete_account', requireLogin, (req, res) => {
+    User.remove({email: req.user.email}, (err, user) => {
+	if (err) res.send({error: "Error: god save us now"});
+
+	req.session.reset();
+	res.send({ message: "successfully deleted account for " + req.user.email});
+    });
 });
-*/
+
+
+app.get('/user_info', requireLogin, (req, res) => {
+    res.send(req.user);
+});
+
+//TODO: set up an authenticated way to update critical info such as email, paswd
+app.put("/update_user", requireLogin, (req, res) => {
+
+    var invalidEditFields = ["password", "email", "songs", "__v", "_id"];
+
+    User.findOne({email: req.user.email}, (err, user) => {
+	if(err) req.send({error: "error, bad request, no such user logged in"});
+	
+	for(var v in req.body) {
+	    if(user[v] 
+	       && req.body[v] != user[v]) {
+
+		if(invalidEditFields.includes(v)) {
+		    res.send({Error: "Cannot edit field " + v + ". protected"});
+		    return;
+		}
+
+		var newVarObj = {};
+		newVarObj[v] = req.body[v];
+		User.update({email: user.email}, {$set: newVarObj}, (err, res) => {
+		    if(err) res.send({error: "Error: could not edit a field requested"});
+		    console.log("the paramater " + v + " success changed");
+		});	
+	    }
+	}
+
+	//return the updated user object
+	User.findOne({email: user.email}, (errTwo, updated) => {
+	    if(errTwo) res.send({error: "There has been an error fetching the updated object"});
+
+//	    updated.password = undefined;
+	    res.send(updated);
+	});
+    });
+});
+
+
 /*
   Make this app live by binding it to a port
 */
@@ -143,6 +237,7 @@ var server = app.listen("3000", function () {
 //export the server variable to our module, so that when somthing else 
 //wants to use server, all it needs to do is var app = require("app.js"); app.xxx...
 module.exports = server;
+
 
 
 
